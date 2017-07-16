6d3392f179140891c5fd0f9afefe3705a982e747
Aishwarya <aishwarya.t2311@gmail.com>
2017-07-16 11:12:25 +0530

    Add files via upload

---
+++ SumOfNaturalNumbers.java
@@ -0,0 +1,16 @@
+package map;
+
+import java.util.Scanner;
+
+public class SumOfNaturalNumbers {
+	public static void main(String[] args){
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int sum=0;
+				for(int i=1;i<=n;i++){
+					sum+=i;
+		}
+				System.out.println(sum);
+				s.close();
+	}
+}
